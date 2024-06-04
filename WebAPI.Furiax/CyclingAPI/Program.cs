f40d48673e17fefd6e2153e30cefbc7d2f86c44e
furiax <carlmalfliet@proximus.be>
2024-06-04 20:38:02 +0200

    registered context to program.cs

--- WebAPI.Furiax/CyclingAPI/Program.cs
+++ WebAPI.Furiax/CyclingAPI/Program.cs
@@ -1,8 +1,13 @@
+using CyclingAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<CyclingDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -23,4 +28,8 @@ app.Run();
  * installed nuget packages
  * clean up program.cs, removing unneccessary code
  * added code to program.cs to add controllers
+ * created context file
+ * added connectionstring to appsettings.json
+ * registered context to program.cs
+ * 
  */

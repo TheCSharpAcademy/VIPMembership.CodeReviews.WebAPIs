ed6f87c27f7e42f5225c351dfbfaca3be78b0549
furiax <carlmalfliet@proximus.be>
2024-06-04 20:45:11 +0200

    created model

---
+++ WebAPI.Furiax/CyclingAPI/Models/CyclingTrip.cs
@@ -0,0 +1,14 @@
+﻿namespace CyclingAPI.Models
+{
+    public class CyclingTrip
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string StartingLocation { get; set; } = string.Empty;
+        public string EndLocation { get; set; } = string.Empty ;
+        public int Distance { get; set; }
+        public TimeSpan Duration { get; set; }
+        public double AvarageSpeed { get; set; }
+        public int AltitudeMeters { get; set; }
+    }
+}
